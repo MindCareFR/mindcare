d4c6e5c619d020f6d0c8f5a99b80aa7d38ba9976
Tanguy Gibrat <twnguy@MacBook-Pro-de-Tanguy.local>
2024-05-29 16:30:08 +0200

    feat: IUserData

---
+++ front/src/app/interfaces/userData.ts
@@ -0,0 +1,14 @@
+export interface IUserData {
+    gender: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    birthdate: string;
+    phone: string;
+    address: string;
+    city: string;
+    zipcode: string;
+    country: string;
+}
