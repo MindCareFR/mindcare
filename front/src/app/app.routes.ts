f385292c631aca8d0c8c2511e537cb8d0f5777fd
Tanguy Gibrat <gibratanguy@icloud.com>
2024-07-04 15:11:03 +0200

    route: signup route

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthLoginComponent } from './pages/auth-login/auth-login.component';
+import { AuthSignupComponent } from './pages/auth-signup/auth-signup.component';
 import { AppHomeComponent } from './pages/app-home/app-home.component';
 
 export const routes: Routes = [
   { path: 'auth/login', component: AuthLoginComponent },
+  { path: 'auth/signup', component: AuthSignupComponent },
   { path: '', component: AppHomeComponent },
   { path: '**', redirectTo: '' }
 ];
