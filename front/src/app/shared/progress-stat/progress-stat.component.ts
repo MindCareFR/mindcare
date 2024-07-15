378bab7654554bd53ce6c3766f5d7eec203d23a5
Tanguy Gibrat <gibratanguy@icloud.com>
2024-07-15 16:40:58 +0200

    init: progress stats component

---
+++ front/src/app/shared/progress-stat/progress-stat.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-stat',
+  standalone: true,
+  imports: [],
+  templateUrl: './progress-stat.component.html',
+})
+export class ProgressStatComponent {
+
+}
