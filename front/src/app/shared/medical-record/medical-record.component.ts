7c834115a0889c04af28e9b29cf9d74c7519a7a6
Tanguy Gibrat <gibratanguy@icloud.com>
2024-07-15 16:40:31 +0200

    init: medical record component

---
+++ front/src/app/shared/medical-record/medical-record.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-record',
+  standalone: true,
+  imports: [],
+  templateUrl: './medical-record.component.html',
+})
+export class MedicalRecordComponent {
+
+}
