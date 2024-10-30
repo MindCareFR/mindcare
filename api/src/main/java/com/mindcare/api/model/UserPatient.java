5288993401113a8f402b6cf50bcee571f62ffc57
twnguydev <154362306+twnguydev@users.noreply.github.com>
2024-10-30 17:05:22 +0100

    Draft/T-0 : added missing data for users close #19 #20 (#27)
    * fix(api): database table name for 'user'
    
    * draft(front): linting for existing features
    
    * draft(front): linted all existing files
    
    * draft(test): added e2e tests to form component, test config
    
    * draft(test): added e2e test to video component
    
    * draft(test): added e2e test to conference component
    
    * draft(test): added e2e test to signup component, formatted files
    
    * draft(test): added e2e tests on login component
    
    * draft(api): added missing data for users #19 #20

--- api/src/main/java/com/mindcare/api/model/UserPatient.java
+++ api/src/main/java/com/mindcare/api/model/UserPatient.java
@@ -5,7 +5,6 @@ import lombok.EqualsAndHashCode;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import jakarta.persistence.Column;
-import java.time.LocalDate;
 
 @Entity
 @Table(name = "user_patient")
@@ -15,9 +14,6 @@ public class UserPatient extends User {
 
   @Column(nullable = false)
   private String gender;
-
-  @Column(nullable = false)
-  private LocalDate birthDate;
 
   @Column(nullable = false)
   private Boolean isAnonymous = false;
