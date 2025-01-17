da866913936bd87adcc4b18e6497a0438a583f99
Dylan Bouraoui <151518436+dylanepitech@users.noreply.github.com>
2025-01-17 14:35:37 +0100

    Devops (#46)
    * fix(gitignore): rajout .idea pour éviter les probleme avec aboubakr
    
    * push rapide
    
    * push
    
    * Ajout de kibana, elastic et portnair
    
    ---------
    
    Co-authored-by: NightDev83 <dylan.bouraoui83@gmail.com>

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,17 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    provideRouter(routes),
-    importProvidersFrom(BrowserModule),
-    importProvidersFrom(ReactiveFormsModule),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
