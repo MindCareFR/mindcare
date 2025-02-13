1f68455aab3b49715f84b74d045da1a8e59d3740
Dylan Bouraoui <151518436+dylanepitech@users.noreply.github.com>
2025-02-13 10:30:46 +0100

    push laravel and new configuration (#56)
    Co-authored-by: DylanBro <dylan.bouraoui83@gmail.com>

--- api/src/main/java/com/mindcare/api/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.mindcare.api.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-  public DuplicateEmailException(String message) {
-      super(message);
-  }
-}
