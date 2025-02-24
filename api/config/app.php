b97f31753cc2007286fad05ab9fadbb7fc438902
Saskoue <romain.farinacci@hotmail.com>
2025-02-24 10:01:52 +0100

    fix(Enc): fixed issues with .env vars

--- api/config/app.php
+++ api/config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
+    'encryption_key' => env('APP_ENCRYPTION_KEY'),
 
     /*
     |--------------------------------------------------------------------------
