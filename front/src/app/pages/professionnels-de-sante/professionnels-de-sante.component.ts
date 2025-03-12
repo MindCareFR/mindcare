770b8df259533516d73851b9299fbbbf3cffdafd
jyhgyh <rinat.hirfanov@epitech.eu>
2025-03-12 18:22:37 +0200

    feat: variables, responsible #71

--- front/src/app/pages/professionnels-de-sante/professionnels-de-sante.component.ts
+++ front/src/app/pages/professionnels-de-sante/professionnels-de-sante.component.ts
@@ -5,7 +5,7 @@ import { AvatarModule } from 'primeng/avatar';
 import { AvatarGroupModule } from 'primeng/avatargroup';
 import { ButtonModule } from 'primeng/button';
 import { NavbarComponent } from '@components/header/header.component';
-
+import { FooterComponent } from '@components/footer/footer.component';
 
 
 @Component({
@@ -18,6 +18,7 @@ import { NavbarComponent } from '@components/header/header.component';
     AvatarGroupModule,
     ButtonModule,
     NavbarComponent,
+    FooterComponent,
   ],
   templateUrl: './professionnels-de-sante.component.html',
 })
@@ -27,5 +28,8 @@ export class ProfessionnelsDeSanteComponent {
   note: number = 9;
   ans: number = 5;
   diplome: string = "BAC+3";
+  Approche: string = "Gestalt-thérapie, approche systémique de la famille";
+  Travaux: string = "Peur et crises d'angoisse, Dépendance émotionnelle, Stress";
+  histoire: string = "Je suis psychologue clinicienne, Gestalt thérapeute. J'aide les gens a « faire le tri », a résoudre les problémes actuels et les difficultés dans les relations avec les autres, a apporter"
 
 }
