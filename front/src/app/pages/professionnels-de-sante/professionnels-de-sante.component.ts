9a8f0edad05a67d22e0780a4298c763cbde7450a
jyhgyh <rinat.hirfanov@epitech.eu>
2025-03-14 17:09:37 +0200

    feat: pagination and finished the cards #71

--- front/src/app/pages/professionnels-de-sante/professionnels-de-sante.component.ts
+++ front/src/app/pages/professionnels-de-sante/professionnels-de-sante.component.ts
@@ -31,5 +31,75 @@ export class ProfessionnelsDeSanteComponent {
   Approche: string = "Gestalt-thérapie, approche systémique de la famille";
   Travaux: string = "Peur et crises d'angoisse, Dépendance émotionnelle, Stress";
   histoire: string = "Je suis psychologue clinicienne, Gestalt thérapeute. J'aide les gens a « faire le tri », a résoudre les problémes actuels et les difficultés dans les relations avec les autres, a apporter"
+  currentPage = 1;
+  pageSize = 12;
+  doctors = [
+    {
+      name: 'Dr. Alice',
+      note: 9,
+      ans: 15,
+      diplome: 'PhD',
+      Approche: 'Cognitive Therapy',
+      Travaux: 'Anxiety, Depression',
+      histoire: "Je suis psychologue clinicienne, Gestalt thérapeute. J'aide les gens a « faire le tri », a résoudre les problémes actuels et les difficultés dans les relations avec les autres, a apporter",
+      image: 'https://picsum.photos/200/300',
+      activeTab: 'principal'
+    },
+    { name: 'Dr. Bob',
+      note: 8,
+      ans: 10,
+      diplome: 'MD',
+      Approche: 'Behavioral Therapy',
+      Travaux: 'PTSD, Stress',
+      histoire: 'Former military psychiatrist',
+      image: 'https://picsum.photos/200/300',
+      activeTab: 'principal' },
+    { name: 'Dr. Bob',
+      note: 8,
+      ans: 10,
+      diplome: 'MD',
+      Approche: 'Behavioral Therapy',
+      Travaux: 'PTSD, Stress',
+      histoire: 'Former military psychiatrist',
+      image: 'https://picsum.photos/200/300',
+      activeTab: 'principal'
+    },
+    { name: 'Dr. Bob',
+      note: 8,
+      ans: 10,
+      diplome: 'MD',
+      Approche: 'Behavioral Therapy',
+      Travaux: 'PTSD, Stress',
+      histoire: 'Former military psychiatrist',
+      image: 'https://picsum.photos/200/300',
+      activeTab: 'principal'
+    },
+    { name: 'Dr. Bob',
+      note: 8,
+      ans: 10,
+      diplome: 'MD',
+      Approche: 'Behavioral Therapy',
+      Travaux: 'PTSD, Stress',
+      histoire: 'Former military psychiatrist',
+      image: 'https://picsum.photos/200/300',
+      activeTab: 'principal'
+    },
+  ];
 
+  get paginatedDoctors() {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    return this.doctors.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage * this.pageSize < this.doctors.length) {
+      this.currentPage++;
+    }
+  }
 }
