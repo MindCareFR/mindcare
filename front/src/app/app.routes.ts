dfafedb44000d19da88af811aaaf6c41d0a603ab
jyhgyh <rinat.hirfanov@epitech.eu>
2025-09-09 14:23:10 +0300

    fix: rout to professionnels(#71)

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -22,7 +22,7 @@ export const routes: Routes = [
   { path: 'auth/signup', component: AuthSignupComponent },
   { path: 'conference', component: ConferenceComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'professionnels', component: ProfessionnelsDeSanteComponent },
+  { path: 'professionals', component: ProfessionnelsDeSanteComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
